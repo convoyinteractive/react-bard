f7b6c250c2c83c178b0f24125ff617388df2ca40
Tobias Thaden <hello@tobiasthaden.com>
2022-10-10 16:47:34 +0200

    Export as module (#8)

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,12 @@ import typescript from '@rollup/plugin-typescript';
 export default {
     input: 'src/index.tsx',
     output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        exports: 'named',
-        sourcemap: false,
-        strict: false
-      }
+        {
+            file: pkg.main,
+            format: 'es',
+            exports: 'named',
+            sourcemap: true
+        }
     ],
     plugins: [
       typescript()
